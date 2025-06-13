80a5c04293c05d9c767ebfc30fa99fa5910cd0ce
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-06-13 11:29:43 +0300

    First Commit

---
+++ lib/fruits_hub.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:fruits_hub/core/helper/functions/on_generate_routes.dart';
+import 'package:fruits_hub/features/splash/presentation/views/splash_view.dart';
+import 'package:fruits_hub/generated/l10n.dart';
+
+class FruitsHub extends StatelessWidget {
+  const FruitsHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: "Cairo"),
+      onGenerateRoute: onGenerateRoute,
+      debugShowCheckedModeBanner: false,
+      initialRoute: SplashView.routeName,
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: const Locale('ar'),
+      home: SplashView(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/fruits_hub.dart';
+
+void main() {
+  runApp(const FruitsHub());
+}
