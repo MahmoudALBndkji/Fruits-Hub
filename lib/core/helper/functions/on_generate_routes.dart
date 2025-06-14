a043a52e8d479d2185ae8051491fe61d3db9c0d3
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-06-14 11:40:12 +0300

    Add Auth Routes

--- lib/core/helper/functions/on_generate_routes.dart
+++ lib/core/helper/functions/on_generate_routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fruits_hub/features/auth/presentation/views/login_view.dart';
+import 'package:fruits_hub/features/auth/presentation/views/signin_view.dart';
+import 'package:fruits_hub/features/auth/presentation/views/signup_view.dart';
 import 'package:fruits_hub/features/on-boarding/presentation/views/on_boarding_view.dart';
 import 'package:fruits_hub/features/splash/presentation/views/splash_view.dart';
 
@@ -9,8 +10,10 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const SplashView());
     case OnBoardingView.routeName:
       return MaterialPageRoute(builder: (context) => const OnBoardingView());
-    case LoginView.routeName:
-      return MaterialPageRoute(builder: (context) => const LoginView());
+    case SignInView.routeName:
+      return MaterialPageRoute(builder: (context) => const SignInView());
+    case SignupView.routeName:
+      return MaterialPageRoute(builder: (context) => const SignupView());
     default:
       return MaterialPageRoute(builder: (context) => Scaffold());
   }
