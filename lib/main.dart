ff86612eec7dedb2fdc663c1bba6152199279848
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-06-14 16:27:36 +0300

    Add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_hub/fruits_hub.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fruits_hub/firebase_options.dart';
+import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Prefs.init();
   runApp(const FruitsHub());
 }
