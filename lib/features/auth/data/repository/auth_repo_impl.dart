aae5830001aac2bb5cd13a9a9fb188eebfc91489
MahmoudALBndkji <mahmoudalbndkji@gmail.com>
2025-06-19 21:14:04 +0300

    Auth Repository

---
+++ lib/features/auth/data/repository/auth_repo_impl.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub/core/errors/failure.dart';
+import 'package:fruits_hub/core/errors/exceptions.dart';
+import 'package:fruits_hub/core/services/firebase_auth_service.dart';
+import 'package:fruits_hub/features/auth/data/models/user_model.dart';
+import 'package:fruits_hub/features/auth/domain/entites/user_entity.dart';
+import 'package:fruits_hub/features/auth/domain/repository/auth_repo.dart';
+
+class AuthRepoImpl implements AuthRepo {
+  final FirebaseAuthService firebaseAuthService;
+  AuthRepoImpl({required this.firebaseAuthService});
+
+  @override
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      final user = await firebaseAuthService.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return Right(UserModel.fromFirebaseUser(user));
+    } on CustomException catch (e) {
+      return Left(ServerFailure(e.message));
+    } catch (e) {
+      return Left(ServerFailure('An error occurred. Please try again later.'));
+    }
+  }
+}
